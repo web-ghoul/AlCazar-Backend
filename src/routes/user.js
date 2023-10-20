f9f39bd16cbbb4e53f7a0fa1088635e90a002d7c
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-10-20 14:14:08 +0300

    Done

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,8 +6,9 @@ const { webHook, messagingWebHook } = require("../controllers/facebookMessangerB
 
 router.route("/:id").get(getUser)
 
-router.route("webhook").post(webHook)
+router.route("/messengerBot/webhook").post(webHook)
 
-router.route("webhook").post(messagingWebHook)
+router.route("/messengerBot/webhook").get(messagingWebHook)
 
 module.exports = router
+// curl -X GET "localhost:3000api/user/webhook?hub.verify_token=kinyki&hub.challenge=CHALLENGE_ACCEPTED&hub.mode=subscribe"

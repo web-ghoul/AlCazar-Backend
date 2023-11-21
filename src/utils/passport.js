6448bbddad810f1e118a7929fba4f449be519381
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-11-21 02:27:35 +0200

    Keep Changes

--- src/utils/passport.js
+++ src/utils/passport.js
@@ -7,7 +7,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/google/callback",
+      callbackURL: "/api/auth/google/callback",
       scope: ["profile", "email"],
     },
     function (accessToken, refreshToken, profile, callback) {

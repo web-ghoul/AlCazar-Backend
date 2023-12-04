9fe7e0e303d5181b0a60aff61f45030154280735
webGhoul <84246173+web-ghoul@users.noreply.github.com>
2023-12-04 21:29:56 +0200

    test version

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,5 +1,5 @@
 const User = require("../models/user");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { sendMail } = require("../utils/sendMail");
 const { welcomeMail } = require("../mails/welcomeMail");
